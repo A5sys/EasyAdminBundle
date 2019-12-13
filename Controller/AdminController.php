7a38bc3e3514a22e11d9b92e426d41faf3879ab8
Benjamin Dutriaux <bdutriaux@a5sys.com>
2019-12-13 17:59:00 +0100

    resolve error on form submit

--- Controller/AdminController.php
+++ Controller/AdminController.php
@@ -603,13 +603,28 @@ class AdminController extends Controller
      */
     protected function createEntityFormBuilder($entity, $view)
     {
-        $formOptions = $this->entity[$view]['form_options'];
-        $formOptions['entity'] = $this->entity['name'];
-        $formOptions['view'] = $view;
+        $formOptions = $this->executeDynamicMethod('get<EntityName>EntityFormOptions', array($entity, $view));
 
         $formType = $this->useLegacyFormComponent() ? 'easyadmin' : 'JavierEguiluz\\Bundle\\EasyAdminBundle\\Form\\Type\\EasyAdminFormType';
 
         return $this->get('form.factory')->createNamedBuilder(strtolower($this->entity['name']), $formType, $entity, $formOptions);
+    }
+
+    /**
+     * Retrieves the list of form options before sending them to the form builder.
+     * This allows adding dynamic logic to the default form options.
+     *
+     * @param object $entity
+     * @param string $view
+     *
+     * @return array
+     */
+    protected function getEntityFormOptions($entity, $view)
+    {
+        $formOptions = $this->entity[$view]['form_options'];
+        $formOptions['entity'] = $this->entity['name'];
+        $formOptions['view'] = $view;
+        return $formOptions;
     }
 
     /**
